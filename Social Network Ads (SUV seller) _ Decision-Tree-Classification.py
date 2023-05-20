9de52963fb82190d8b8cf1f59919bad8e42b6090
MiraminBagheri <122800493+MiraminBagheri@users.noreply.github.com>
2023-05-20 18:29:21 +0330

    Decision Tree

---
+++ Social Network Ads (SUV seller) _ Decision-Tree-Classification.py
@@ -0,0 +1,116 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# print(X_train)
+# print(y_train)
+# print(X_test)
+# print(y_test)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# print(X_train)
+# print(X_test)
+
+# Training the Decision Tree Classification model on the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(class_weight= 'balanced', criterion= 'entropy', max_depth= 5, max_features= None,
+                                    min_samples_leaf= 4, min_samples_split= 2, splitter= 'best', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'criterion': ['gini', 'entropy'],
+                'splitter': ['best', 'random'],
+                'max_depth': [3, 5, 7, None],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4],
+                'max_features': ['auto', 'sqrt', 'log2', None],
+                'class_weight': [None, 'balanced']}]
+grid_search = GridSearchCV(estimator = classifier,
+                            param_grid = parameters,
+                            scoring = 'accuracy',
+                            cv = 10,
+                            n_jobs = -1)
+grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
+print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+print("Best Parameters:", best_parameters)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+# Predicting a new result
+print(classifier.predict(sc.transform([[30,87000]])))
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+# print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print('\n Test-Set Accuracy Score: ', accuracy_score(y_test, y_pred))
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], color = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classification (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], color = ListedColormap(('red', 'green'))(i), label = j)
+plt.scatter(30, 87000, color = 'yellow' if classifier.predict(sc.transform([[30,87000]])) == 0 else 'blue', marker='*', label='Single Pred', s=100)
+plt.title('Decision Tree Classification (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Save the output (result) in CSV file :
+Header = ['Module_Name', 'Feature Scaled', 'Accuracy', 'Standard Deviation', 'Test-Set Accuracy Score']
+output= [['Decision Tree', 'Yes', accuracies.mean()*100, accuracies.std()*100, accuracy_score(y_test, y_pred)]]
+output = np.array(output)
+Output = pd.DataFrame(output)
+try :
+    pd.read_csv('C:/Users/Miramin_LPC/Desktop/PythonS/Projects/Social Network Ads (SUV seller)/Outputs.csv')
+    Output.to_csv('C:/Users/Miramin_LPC/Desktop/PythonS/Projects/Social Network Ads (SUV seller)/Outputs.csv', mode='a', index=False, header=False)
+except:
+    Output.to_csv('C:/Users/Miramin_LPC/Desktop/PythonS/Projects/Social Network Ads (SUV seller)/Outputs.csv', mode='w', sep=',', index=False, header=Header)
